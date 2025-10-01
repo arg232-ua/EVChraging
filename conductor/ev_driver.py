62cb78b5c4c84db45596a7d8ebcea84c9cc720de
Aarón Ruano <arg232@alu.ua.es>
2025-10-01 13:23:38 +0200

    Actualización de ficheros

--- conductor/ev_driver.py
+++ conductor/ev_driver.py
@@ -1,20 +1,38 @@
-from compartido.config import KAFKA_SERVER
-from compartido.kafka_helper import obtener_productor, obtener_consumidor
+import sys
+import json
+from kafka import KafkaProducer, KafkaConsumer
 import time # Para monitorear los eventos que van ocurriendo
 import json
 import threading
 
+def obtener_productor(servidor_kafka): # Crea un nuevo productor de Kafka
+    return KafkaProducer(
+        bootstrap_servers=[servidor_kafka], # Dirección del servidor de Kafka
+        value_serializer=lambda v: json.dumps(v).encode('utf-8') # Forma de codificar los mensajes
+    )
+
+def obtener_consumidor(topico, grupo_id, servidor_kafka): # Crea un nuevo consumidor de Kafka
+    return KafkaConsumer(
+        topico,
+        bootstrap_servers=[servidor_kafka], # Dirección del servidor de Kafka
+        value_deserializer=lambda v: json.loads(v.decode('utf-8')), # Forma de decodificar los mensajes
+        grupo_id = grupo_id, # Identificador del grupo de consumidores
+        auto_offset_reset='earliest' # Comenzar a leer desde el principio del tópico
+    )
+
 class EvDriver:
-    def __init__(self, driver_id):
+    def __init__(self, driver_id, servidor_kafka):
         self.driver_id = driver_id
-        self.productor = obtener_productor()
+        self.productor = obtener_productor(servidor_kafka)
         self.verificado = False
 
-        self.verificar_driver() # Comprobamos si el conductor esta registrado en la BD
+        print("PRUEBA: VOY A VERIFICAR") # borrrarrrrr #####
 
-        print(f"Conductor {driver_id} inicializado y conectado a Kafka: {KAFKA_SERVER}")
+        if self.verificar_driver(): # Comprobamos si el conductor esta registrado en la BD
 
-        self.escuchar_respuestas()
+            print(f"Conductor {driver_id} inicializado y conectado a Kafka: {servidor_kafka}")
+
+            self.escuchar_respuestas(servidor_kafka)
     
     def verificar_driver(self):
         mensaje = {
@@ -27,12 +45,14 @@ class EvDriver:
             self.productor.send('conductor', mensaje)
             self.productor.flush() # Aseguramos que el mensaje se envie
             print(f"Verificando si el conductor {self.driver_id} esta registrado en la Base de Datos...")
+            return True
         except Exception as e:
             print(f"Error al verificar al Conductor: {e}")
+            return False
 
-    def escuchar_respuestas(self):
+    def escuchar_respuestas(self, servidor_kafka):
         def escuchar():
-            consumidor = obtener_consumidor('respuestas_conductor', f'conductor-id-{self.driver_id}')
+            consumidor = obtener_consumidor('respuestas_conductor', f'conductor-id-{self.driver_id}', servidor_kafka)
 
             for msg in consumidor:
                 respuesta = msg.value
@@ -49,7 +69,7 @@ class EvDriver:
         hilo_escucha = threading.Thread(target=escuchar, daemon=True)
         hilo_escucha.start()
     
-    def solicitar_recarga(self, cp_id):
+    def solicitar_recarga(self, cp_id): # Solicito recarga a CP
         if not self.verificado: # Si el conductor no esta en la BBDD
             print("Conductor no verificado. No puede solicitar recargas")
             return False
@@ -69,3 +89,20 @@ class EvDriver:
         except Exception as e:
             print(f"Error al solicitar recarga: {e}")
             return False
+        
+def main():
+    if len(sys.argv) < 3:
+        print("ERROR: Argumentos incorrectos")
+        print("Argumentos correctos: python EV_Driver.py <IP:puerto_broker> <id_driver>")
+        sys.exit(1) # Devuelve error
+    
+    servidor_kafka = sys.argv[1]
+    id_driver = sys.argv[2]
+
+    print(f"Prueba: {servidor_kafka}, {id_driver}") # BORRRAR
+
+    ev_driver = EvDriver(id_driver, servidor_kafka)
+
+
+if __name__ == "__main__":
+    main()
